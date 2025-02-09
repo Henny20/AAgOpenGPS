02609a55487c725feb54e8746cecc26503df1304
Henny20 <47795195+Henny20@users.noreply.github.com>
2025-02-09 16:18:28 +0100

    Add files via upload

--- AgOpenGPS/ViewModels/MainViewModel.cs
+++ AgOpenGPS/ViewModels/MainViewModel.cs
@@ -1,1 +1,9 @@
+﻿using CommunityToolkit.Mvvm.ComponentModel;
 
+namespace AgOpenGPS.ViewModels;
+
+public partial class MainViewModel : ViewModelBase
+{
+    [ObservableProperty]
+    private string _greeting = "Welcome to Avalonia!";
+}
---
+++ AgOpenGPS/ViewModels/ViewModelBase.cs
@@ -0,0 +1,7 @@
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+
+namespace AgOpenGPS.ViewModels;
+
+public abstract class ViewModelBase : ObservableObject
+{
+}
