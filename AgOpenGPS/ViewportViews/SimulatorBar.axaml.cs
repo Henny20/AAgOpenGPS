4c256209280bdeef0ade2fef4651b0f39435062a
Henny20 <47795195+Henny20@users.noreply.github.com>
2025-02-09 16:13:39 +0100

    Add files via upload

---
+++ AgOpenGPS/ViewportViews/SimulatorBar.axaml.cs
@@ -0,0 +1,18 @@
+﻿using Avalonia.Controls;
+using Avalonia.Markup.Xaml;
+
+namespace AgOpenGPS.ViewportViews
+{
+    public partial class SimulatorBar : UserControl
+    {
+        public SimulatorBar()
+        {
+            InitializeComponent();
+        }
+        
+        private void InitializeComponent()
+        {
+            AvaloniaXamlLoader.Load(this);
+        }
+    }
+}
---
+++ AgOpenGPS/ViewportViews/ViewportOverlay.axaml.cs
@@ -0,0 +1,18 @@
+﻿using Avalonia.Controls;
+using Avalonia.Markup.Xaml;
+
+namespace AgOpenGPS.ViewportViews
+{
+    public partial class ViewportOverlay : UserControl
+    {
+        public ViewportOverlay()
+        {
+            InitializeComponent();
+        }
+        
+        private void InitializeComponent()
+        {
+            AvaloniaXamlLoader.Load(this);
+        }
+    }
+}
