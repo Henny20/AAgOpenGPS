4ab0a1dd22215dd15ec8bf720994f1ccd1f122ae
Henny20 <47795195+Henny20@users.noreply.github.com>
2025-02-09 22:52:24 +0100

    Update MainViewModel.cs

--- AgOpenGPS/ViewModels/MainViewModel.cs
+++ AgOpenGPS/ViewModels/MainViewModel.cs
@@ -6,4 +6,11 @@ public partial class MainViewModel : ViewModelBase
 {
     [ObservableProperty]
     private string _greeting = "Welcome to Avalonia!";
+
+    [ObservableProperty]
+    private SolidColorBrush _buttonColor = new SolidColorBrush(Color.FromRgb(243, 243, 243));
+    
+    [ObservableProperty]
+    private bool _buttonVisible = true;
+    
 }
